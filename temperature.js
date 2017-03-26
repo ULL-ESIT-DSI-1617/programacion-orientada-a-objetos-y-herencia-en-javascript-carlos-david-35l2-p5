0aaef0f6c1097dc6497d6a6685578d46eaf3872e
alu0100851236 <alu0100851236@ull.edu.es>
2017-03-26 20:59:37 +0100

    C3 - #4 Se crea la clase Temperatura

--- temperature.js
+++ temperature.js
@@ -5,6 +5,12 @@ function Medida (valor, tipo) {
   this.tipo_ = tipo;
 }
 
+function Temperatura (valor, tipo){
+  Medida.call(this, valor, tipo);
+}
+
+Temperatura.prototype = new Medida();
+
 function calculate() {
   var result;
   var temp = original.value;
