336cccd1c94d784ccbd529ae6264c069ea6c5bcc
alu0100851236 <alu0100851236@ull.edu.es>
2017-03-26 21:24:26 +0100

    #6 - Se admiten también los grados Kelvin

--- temperature.js
+++ temperature.js
@@ -11,12 +11,40 @@ function Temperatura (valor, tipo){
 
 Temperatura.prototype = new Medida();
 
-Temperatura.prototype.convertirF = function(){
+Medida.prototype.get_valor = function(){return this.valor_;}
+Medida.prototype.get_tipo = function(){return this.tipo_;}
+
+Medida.prototype.set_valor = function(valor){this.valor_ = valor;}
+Medida.prototype.set_tipo = function(tipo){this.tipo_ = tipo;}
+
+//Convertir Celsius a Farenheit
+Temperatura.prototype.convertirC_F = function(){
   return ((this.get_valor()*9)/5)+32;
 }
-
-Temperatura.prototype.convertirC = function(){
+//Convertir Celsius a Kelvin
+Temperatura.prototype.convertirC_K = function(){
+  return (this.get_valor()+273.15);
+}
+//Convertir Farenheit a Celsius
+Temperatura.prototype.convertirF_C = function(){
   return ((this.get_valor()-32)*5)/9;
+}
+//Convertir Farenheit a Kelvin
+Temperatura.prototype.convertirF_K = function(){
+  return (((this.get_valor()-32)*5)/9)+273.15;
+}
+//Convertir Kelvin a Celsius
+Temperatura.prototype.convertirK_C = function(){
+  return (this.get_valor()-273.15);
+}
+//Convertir Kelvin a Farenheit
+Temperatura.prototype.convertirK_F = function(){
+  return (((this.get_valor()-273.15)*9)/5)+32;
+}
+
+Temperatura.prototype.mostrar = function(){
+  var result = this.get_valor() + " " + this.get_tipo();
+  return result;
 }
 
 function calculate() {
